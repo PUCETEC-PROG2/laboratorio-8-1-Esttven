08cded78f6f1e30a8fdef2a91d3e25efff8a039b
Esteban Arteaga <mattesteban@outlook.es>
2024-08-03 21:10:31 -0500

    Laboratorio 10

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("pokemon/<int:pokemon_id>/", views.pokemon, name="pokemon"),
     path("add_pokemon/", views.add_pokemon, name='add_pokemon'),
-    path("login/", views.CustomLoginView.as_view(), name='login'),
     path("edit_pokemon/<int:id>/", views.edit_pokemon, name='edit_pokemon'),
     path("delete_pokemon/<int:id>/", views.delete_pokemon, name='delete_pokemon'),
+    path("trainer/<int:trainer_id>/", views.trainer, name="trainer"),
+    path("add_trainer/", views.add_trainer, name='add_trainer'),
+    path("edit_trainer/<int:id>/", views.edit_trainer, name='edit_trainer'),
+    path("delete_trainer/<int:id>/", views.delete_trainer, name='delete_trainer'),
+    path("login/", views.CustomLoginView.as_view(), name='login'),
 ]
--- pokedex/views.py
+++ pokedex/views.py
@@ -2,14 +2,24 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.template import loader
 from .models import Pokemon
+from .models import Trainer
 from .forms import PokemonForm
+from .forms import TrainerForm
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.decorators import login_required
+from datetime import date
 
 def index(request):
     pokemons = Pokemon.objects.order_by('type')
+    trainers = Trainer.objects.order_by('first_name')
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons}, request))
+    for trainer in trainers:
+        trainer.age = calculate_age(trainer.birth_date)
+    context = {
+        'pokemons': pokemons,
+        'trainers': trainers,
+    }
+    return HttpResponse(template.render(context, request))
 
 def pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(id=pokemon_id)
@@ -47,6 +57,48 @@ def delete_pokemon(request, id):
     pokemon = get_object_or_404(Pokemon, pk=id)
     pokemon.delete()
     return redirect('pokedex:index')
+
+def trainer(request, trainer_id):
+    trainer = Trainer.objects.get(id=trainer_id)
+    template = loader.get_template('display_trainer.html')
+    context = {
+        'trainer': trainer
+    }
+    return HttpResponse(template.render(context, request))
+
+@login_required
+def add_trainer(request):
+    if request.method == 'POST':
+        form = TrainerForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = TrainerForm()
+    return render(request, 'trainer_form.html', {'form': form})
+
+@login_required
+def edit_trainer(request, id):
+    trainer = get_object_or_404(Trainer, pk = id)
+    if request.method == 'POST':
+        form = TrainerForm(request.POST, request.FILES, instance=trainer)
+        if form.is_valid():
+            form.save()
+            return redirect('pokedex:index')
+    else:
+        form = TrainerForm(instance=trainer)
+    return render(request, 'trainer_form.html', {'form': form})
+
+@login_required
+def delete_trainer(request, id):
+    trainer = get_object_or_404(Trainer, pk=id)
+    trainer.delete()
+    return redirect('pokedex:index')
+
+def calculate_age(birth_date):
+    today = date.today()
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    return age
     
 
 class CustomLoginView(LoginView):
